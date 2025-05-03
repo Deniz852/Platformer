2008fbe0ddc55d2a9370aa17fd2e65c615a29c6a
Deniz852 <denisdvcube@gmail.com>
2025-05-03 13:57:13 +0500

    .

---
+++ Assets/Code/EToEnter.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EToEnter : MonoBehaviour
+{
+    public Transform teleportPoint;
+    public GameObject text;
+    private void Start()
+    {
+        text.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        text.SetActive(true);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                other.transform.position = teleportPoint.position;
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        text.SetActive(false);
+    }
+}
